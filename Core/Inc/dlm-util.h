3d4fe9af55c338b6c49af38faede203003aadcc5
olale013 <95550079+olale013@users.noreply.github.com>
2022-04-19 21:53:42 -0500

    minor changes to util functions

--- Core/Inc/dlm-util.h
+++ Core/Inc/dlm-util.h
@@ -10,7 +10,25 @@
 
 #include <stdint.h>
 
-uint8_t append_byte(uint8_t buffer[], uint8_t sz, uint8_t byte);
-uint8_t append_packet(uint8_t buffer[], uint8_t sz, uint32_t timestamp, uint16_t id, void* data, uint8_t dataSize);
+// escapes a byte if necessary then appends it to the provided buffer
+// PARAMETERS:
+// buffer: the byte buffer to append to
+// size: current size of the buffer in bytes
+// byte: byte to append
+// RETURNS:
+// the new buffer size
+uint32_t append_byte(uint8_t buffer[], uint32_t size, uint8_t byte);
+
+// convenience function to append the standard packet structure to a buffer
+// PARAMETERS:
+// buffer: the byte buffer to append to
+// size: current size of the buffer in bytes
+// timestamp: 4 byte timestamp in ms
+// id: 2 byte data ID
+// data: pointer to the data
+// dataSize: size of the data in bytes
+// RETURNS:
+// the new buffer size
+uint32_t append_packet(uint8_t buffer[], uint32_t sz, uint32_t timestamp, uint16_t id, void* data, uint8_t dataSize);
 
 #endif /* INC_DLM_UTIL_H_ */
--- Core/Src/dlm-util.c
+++ Core/Src/dlm-util.c
@@ -8,65 +8,49 @@
 #include "dlm-util.h"
 #include "dlm.h"
 
-// dlm-util.c contains functions that may be used across DLM features
-
-// escapes a byte if necessary, then appends it to the buffer
-// PARAMETERS:
-// - buffer: the buffer to append to
-// - sz: current size of the buffer
-// - byte: byte to append
-// RETURNS: the new buffer size
-uint8_t append_byte(uint8_t buffer[], uint8_t sz, uint8_t byte) {
-    uint8_t size = sz;
+uint32_t append_byte(uint8_t buffer[], uint32_t size, uint8_t byte) {
+    uint32_t sz = size;
 
     // check for a control byte
-    if (byte == 0x7e || byte == 0x7d) {
+    if (byte == START_BYTE || byte == ESCAPE_BYTE) {
         // append an escape byte
-        buffer[size] = 0x7d;
-        size++;
-        // append the escaped byte
-        buffer[size] = byte ^ 0x20;
-        size++;
+        buffer[sz] = ESCAPE_BYTE;
+        sz++;
+        // append the desired byte, escaped
+        buffer[sz] = byte ^ 0x20;
+        sz++;
     } else {
     	// append the raw byte
-        buffer[size] = byte;
-        size++;
+        buffer[sz] = byte;
+        sz++;
     }
 
-    return size;
+    return sz;
 }
 
-// takes packet components, breaks them into bytes, and appends them to a buffer
-// PARAMETERS:
-// - buffer: the buffer to append to
-// - sz: current size of buffer
-// - timestamp: in ms
-// - id
-// - data: pointer to the data
-// - dataSize: size of the data in bytes
-// RETURNS: the new buffer size
-uint8_t append_packet(uint8_t buffer[], uint8_t sz, uint32_t timestamp, uint16_t id, void* data, uint8_t dataSize) {
-	uint8_t size = sz;
+uint32_t append_packet(uint8_t buffer[], uint32_t size, uint32_t timestamp, uint16_t id, void* data, uint8_t dataSize) {
+	uint32_t sz = size;
     uint8_t i;
     uint8_t* bytes;
 
-    buffer[size] = (uint8_t) START_BYTE;
-    size++;
+    buffer[sz] = START_BYTE;
+    sz++;
 
+    // append components with MSB first
+    bytes = (uint8_t*) &(timestamp);
     for (i = sizeof(timestamp); i > 0; i--) {
-        bytes = (uint8_t*) &(timestamp);
-        size = append_byte(buffer, size, bytes[i - 1]);
+        sz = append_byte(buffer, sz, bytes[i - 1]);
     }
 
+    bytes = (uint8_t*) &(id);
     for (i = sizeof(id); i > 0; i--) {
-		bytes = (uint8_t*) &(id);
-		size = append_byte(buffer, size, bytes[i - 1]);
+		sz = append_byte(buffer, sz, bytes[i - 1]);
 	}
 
+    bytes = (uint8_t*) data;
     for (i = dataSize; i > 0; i--) {
-		bytes = (uint8_t*) data;
-		size = append_byte(buffer, size, bytes[i - 1]);
+		sz = append_byte(buffer, sz, bytes[i - 1]);
 	}
 
-    return size;
+    return sz;
 }
